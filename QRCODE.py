49a779f508e8f42ec0a0ae2ca7c12c4da3290031
muchire <vivianmuchire963@gmail.com>
2024-02-02 11:45:50 -0800

    weeks.py guess.py

---
+++ QRCODE.py
@@ -0,0 +1,23 @@
+import pyqrcode
+import os,shutil
+
+title = input("give a title :")
+text = input("qr code should say?")
+
+filesvg = title + '.svg'
+filepng = title+ '.png'
+
+
+url = pyqrcode.create(text)
+
+url.svg(filesvg, scale= 7)
+url.png(filepng, scale= 9)
+
+
+os.mkdir(fr"C:\Users\user\Desktop\{title}")
+
+shutil.move(f"{filesvg}", fr"C:\Users\user\Desktop\{title}")
+shutil.move(f"{filepng}", fr"C:\Users\user\Desktop\{title}")
+
+
+
